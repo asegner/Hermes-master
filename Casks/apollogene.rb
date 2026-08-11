bd2e7d56270de36a32da54e44389049ad17f01dc
Aaron Segner <asegner@gmail.com>
2026-08-11 09:32:50 -0400

    Update for newest release

--- Casks/apollogene.rb
+++ Casks/apollogene.rb
@@ -1,18 +1,20 @@
 cask "apollogene" do
-  version "200b6"
-  sha256 "2bf92aab9473246bed2d5cad52d19d6d13643c13ca06168bf32b5cf4f37e1702"
+  version "201b1"
+  sha256 "233218cb60088a6011d6558a94bed335dcdeba85df769696c23c51d288817c2d"
 
-  url "https://github.com/dtseto/Hermes-master/releases/download/v#{version}/ApolloGene#{version}-n.zip"
+  url "https://github.com/dtseto/Hermes-master/releases/download/v#{version}/ApolloGene-n.app.zip"
   name "ApolloGene"
   desc "Pandora client for Intel and Apple Silicon Macs"
   homepage "https://github.com/dtseto/Hermes-master"
 
   depends_on macos: :big_sur
 
-  app "ApolloGene#{version}-n.app", target: "ApolloGene.app"
+  app "ApolloGene-n.app", target: "ApolloGene.app"
 
   zap trash: [
     "~/Library/Preferences/com.davidseto.ApolloGene.plist",
+    "~/Library/Preferences/com.dtseto.ApolloGene.plist",
     "~/Library/Saved Application State/com.davidseto.ApolloGene.savedState",
+    "~/Library/Saved Application State/com.dtseto.ApolloGene.savedState",
   ]
 end
